eede0ced55538f7226aa2b4c555fdf3a52f44efb
jnzheng <jnzheng@jnzhengs-MacBook-Pro.local>
2020-06-23 12:52:10 +0800

    setup config server, and point to a remote config file in github

---
+++ src/test/java/com/example/jn/demoConfigServer/DemoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jn.demoConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
